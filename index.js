f889746029dd80d8be4092dda8d814203e335d8d
tazmeenilsa <abdull42335@gmail.com>
2025-02-01 10:28:14 +0500

    Hero-section-Edit

---
+++ index.js
@@ -0,0 +1,9 @@
+const textElements = document.querySelectorAll('.contain-2-text');
+
+let index = 0;
+
+setInterval(() => {
+  textElements[index].style.opacity = 0;
+  index = (index + 1) % textElements.length;
+  textElements[index].style.opacity = 1;
+}, 1000);
